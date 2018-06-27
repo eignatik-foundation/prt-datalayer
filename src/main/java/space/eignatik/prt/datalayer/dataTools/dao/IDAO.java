d932d84ec29b8765cacb0bb90a555f9fee577bce
eignatik <evg.ignatik@gmail.com>
2018-06-27 15:06:41 +0200

    Adds basic CRUD operations for data base in generic dao according to  issue #3

--- src/main/java/space/eignatik/prt/datalayer/dataTools/dao/IDAO.java
+++ src/main/java/space/eignatik/prt/datalayer/dataTools/dao/IDAO.java
@@ -1,17 +1,15 @@
 package space.eignatik.prt.datalayer.dataTools.dao;
 
-import org.hibernate.Session;
 import space.eignatik.prt.datalayer.dataTools.session.ISessionFactoryUtil;
-import space.eignatik.prt.datalayer.dataTools.session.SessionFactoryUtil;
 import space.eignatik.prt.datalayer.modelEntities.IEntity;
 
 import java.util.List;
 
 public interface IDAO<T extends IEntity> {
-    T add(T item);
+    void add(T item);
     T get(Class<T> type, int id);
-    List<T> getAll();
-    T delete(int id);
-    T update(T item);
+    List<T> getAll(Class<T> type);
+    void update(T item);
+    T delete(Class<T> type, int id);
     void setFactoryUtil(ISessionFactoryUtil factoryUtil);
 }
--- src/test/java/space/eignatik/prt/datalayer/dataTools/dao/EmployeeDAOTest.java
+++ src/test/java/space/eignatik/prt/datalayer/dataTools/dao/EmployeeDAOTest.java
@@ -1,27 +1,64 @@
 package space.eignatik.prt.datalayer.dataTools.dao;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import space.eignatik.prt.datalayer.dataTools.session.SessionFactoryUtilLocal;
 import space.eignatik.prt.datalayer.modelEntities.Employee;
+import java.util.List;
+
+import static org.testng.Assert.*;
 
 public class EmployeeDAOTest {
     private IDAO<Employee> dao = new UserDAO();
 
     @BeforeClass
-    public void setUp() {
+    public void setUpDB() {
         dao.setFactoryUtil(new SessionFactoryUtilLocal());
     }
 
+    @BeforeMethod
+    public void resetDB() {
+        DAOTestUtil.deleteAllFromTable("Employee");
+    }
+
     @Test
-    public void testWhenAddWithItem_thenItWillBeCreatedInDB() {
+    public void testWhenAddWithItem_thenItCanBeClaimedFromDB() {
         Employee employee = new Employee()
                 .setFname("John")
                 .setLname("Dorian");
         dao.add(employee);
         Employee employeeFromDB = dao.get(Employee.class, employee.getId());
 
-        Assert.assertEquals(employee, employeeFromDB);
+        assertEquals(employeeFromDB, employee);
+    }
+
+    @Test
+    public void testWhenDelete_thenDeletedItemReturned_and_ItIsUnableToClaimFromDB() {
+        Employee employee = new Employee()
+                .setFname("Test")
+                .setLname("Delete");
+        dao.add(employee);
+        assertEquals(dao.delete(Employee.class, employee.getId()), employee);
+        assertNull(dao.get(Employee.class, employee.getId()));
+    }
+
+    @Test
+    public void testWhenGetAll_thenFixedSizeListIsReturned() {
+        int EXPECTED_SIZE = 2;
+        List.of(new Employee().setFname("Yan").setLname("Irwin"),
+                new Employee().setFname("Greg").setLname("One"))
+                .forEach(employee -> dao.add(employee));
+        assertEquals(dao.getAll(Employee.class).size(), EXPECTED_SIZE);
+    }
+
+    @Test
+    public void testWhenUpdate_thenExistingItemIsUpdatedInDB() {
+        Employee employee = new Employee().setFname("Cat").setLname("Johnson");
+        dao.add(employee);
+        dao.update(employee.setFname("Dog").setLname("Catson"));
+        employee = dao.get(Employee.class, employee.getId());
+        assertEquals(employee.getFname(), "Dog");
+        assertEquals(employee.getLname(), "Catson");
     }
 }
