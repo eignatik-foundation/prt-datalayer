be151b0b70d41555b3b47ded747547afc7e96312
eignatik <evg.ignatik@gmail.com>
2018-06-27 12:38:24 +0200

    Adds reconfigured hibernate to use test DB for issue #1

--- src/test/java/space/eignatik/prt/datalayer/dataTools/dao/EmployeeDAOTest.java
+++ src/test/java/space/eignatik/prt/datalayer/dataTools/dao/EmployeeDAOTest.java
@@ -1,11 +1,18 @@
 package space.eignatik.prt.datalayer.dataTools.dao;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import space.eignatik.prt.datalayer.dataTools.session.SessionFactoryUtilLocal;
 import space.eignatik.prt.datalayer.modelEntities.Employee;
 
 public class EmployeeDAOTest {
     private IDAO<Employee> dao = new UserDAO();
+
+    @BeforeClass
+    public void setUp() {
+        dao.setFactoryUtil(new SessionFactoryUtilLocal());
+    }
 
     @Test
     public void testWhenAddWithItem_thenItWillBeCreatedInDB() {
