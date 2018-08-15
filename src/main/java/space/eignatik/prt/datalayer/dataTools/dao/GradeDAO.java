11822c88611a9531c484ecdb413c71bb1c05886c
eignatik <evg.ignatik@gmail.com>
2018-08-15 19:53:15 +0200

    Adds new DAO class for Grade and new test for the DAO

---
+++ src/main/java/space/eignatik/prt/datalayer/dataTools/dao/GradeDAO.java
@@ -0,0 +1,6 @@
+package space.eignatik.prt.datalayer.dataTools.dao;
+
+import space.eignatik.prt.datalayer.modelEntities.Grade;
+
+public class GradeDAO extends GenericDAO<Grade> {
+}
---
+++ src/test/java/space/eignatik/prt/datalayer/dataTools/dao/GradeDAOTest.java
@@ -0,0 +1,66 @@
+package space.eignatik.prt.datalayer.dataTools.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import space.eignatik.prt.datalayer.dataTools.session.SessionFactoryUtilLocal;
+import space.eignatik.prt.datalayer.modelEntities.Grade;
+
+import java.util.List;
+
+import static org.testng.Assert.*;
+import static space.eignatik.prt.datalayer.dataTools.TableNames.GRADE;
+
+public class GradeDAOTest extends BaseDAOTest {
+
+    @Autowired
+    @Qualifier(value = "gradeDAO")
+    private IDAO<Grade> dao;
+
+    @BeforeClass
+    public void setUpDB() {
+        dao.setFactoryUtil(new SessionFactoryUtilLocal());
+    }
+
+    @BeforeMethod
+    public void resetDB() {
+        DAOTestUtil.deleteAllFromTable(GRADE.getEntityName());
+    }
+
+    @Test
+    public void testWhenAddWithItem_thenItCanBeClaimedFromDB() {
+        Grade grade = new Grade().setTitle("Senior Software Engineer");
+        dao.add(grade);
+        Grade gradeFromDB = dao.get(Grade.class, grade.getId());
+
+        assertEquals(gradeFromDB, grade);
+    }
+
+    @Test
+    public void testWhenDelete_thenDeletedItemReturned_and_ItIsUnableToClaimFromDB() {
+        Grade grade = new Grade().setTitle("Senior Software Engineer");
+        dao.add(grade);
+        assertEquals(dao.delete(Grade.class, grade.getId()), grade);
+        assertNull(dao.get(Grade.class, grade.getId()));
+    }
+
+    @Test
+    public void testWhenGetAll_thenFixedSizeListIsReturned() {
+        int EXPECTED_SIZE = 2;
+        List.of(new Grade().setTitle("Senior Software Engineer"),
+                new Grade().setTitle("Regular Software Engineer"))
+                .forEach(grade -> dao.add(grade));
+        assertEquals(dao.getAll(Grade.class).size(), EXPECTED_SIZE);
+    }
+
+    @Test
+    public void testWhenUpdate_thenExistingItemIsUpdatedInDB() {
+        Grade grade = new Grade().setTitle("Regular Software Engineer");
+        dao.add(grade);
+        dao.update(grade.setTitle("Middle Software Engineer"));
+        grade = dao.get(Grade.class, grade.getId());
+        assertEquals(grade.getTitle(), "Middle Software Engineer");
+    }
+}
